0d1d965307cb08756753529e78a686282099847d
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-04-06 14:51:55 -0400

    tasks are run asynch

--- src/main/java/Task/Task.java
+++ src/main/java/Task/Task.java
@@ -5,7 +5,7 @@ package Task;
 
 // given a data file and an operation, runs() the operation on the data file, saves the output to a local temporary file,
 // reports the location of the local temporary file
-public interface Task {
+public interface Task extends Runnable {
 
     public boolean isComplete();
     public void run();
